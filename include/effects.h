5a6024a1b15a671469a80ac91b714018a51dbf0d
Cmaranec <Cmaranec@seznam.cz>
2011-05-26 19:36:09 +0200

    Begin work on particle systems

--- include/effects.h
+++ include/effects.h
@@ -7,8 +7,8 @@ struct Particle
 {
     //Pozice jedne castice
     float x,y,z;
-    //Jeji textura
-    unsigned int textureId;
+    //Jeji model
+    unsigned int modelId;
     //A doba pusobeni na svete
     unsigned int actTime;
 };
@@ -20,21 +20,25 @@ public:
     ~ParticleEmitter();
 
     //Metoda obnoveni
-    bool Update();
+    bool Update(const clock_t diff);
 
-protected:
+    //Staticke promenne
     //Pozice naseho emitteru
     float x,y,z;
     //Vertikalni a horizontalni smer a uhel
     float dirangleh, diranglev, angleh, anglev;
-    //Textura co bude emittovana (nejspis bude nutne nejake pole textur pro hezci finalni vystup)
-    unsigned int textureId;
+    //Model co bude emittovan (nejspis bude nutne nejake pole modelu pro hezci finalni vystup)
+    unsigned int modelId;
     //Maximalni a minimalni dolet castic
     float minrange, maxrange;
     //Cas po jaky maji byt castice vydavany, jejich rychlost a velocity (po jakem case (milisekundy) se vyplivne dalsi castice)
     unsigned int time, speed, velocity;
     //Zdali ma pusobit gravitace
     bool gravity;
+
+    //Pohyblive promenne
+    //celkovy ubehly cas (milisekundy, nejspis)
+    unsigned int totaltime;
 
     //Pracovni promenne
     unsigned int actTime;
@@ -50,15 +54,18 @@ public:
 
     void AddEmitter(float x, float y, float z,
                     float dirangleh, float diranglev, float angleh, float anglev,
-                    unsigned int textureId,
+                    unsigned int modelId,
                     float minrange, float maxrange,
                     unsigned int time, unsigned int speed, unsigned int velocity,
                     bool gravity);
+    void AddEmitter(ParticleEmitter* pEmitter);
 
-    void Update();
+    void Update(const clock_t diff);
 protected:
-    vector<ParticleEmitter*> Emitters;
+    list<ParticleEmitter*> Emitters;
 };
+
+extern EmitterMgr gEmitterMgr;
 
 #endif
 
--- src/effects.cpp
+++ src/effects.cpp
@@ -1,7 +1,30 @@
 #include <effects.h>
 
-bool ParticleEmitter::Update()
+EmitterMgr gEmitterMgr;
+
+ParticleEmitter::ParticleEmitter()
 {
+}
+
+ParticleEmitter::~ParticleEmitter()
+{
+    //odstranit particles
+}
+
+bool ParticleEmitter::Update(const clock_t diff)
+{
+    //Nejdrive pricist k celkovemu casu ten ubehly (milisekundy)
+    totaltime += diff;
+
+    //Pak porovnat celkovy cas, zdali uz ubehnul maximalni cas
+    if(totaltime >= time)
+        return false; //--> odstranit emitter z listu pro update
+    //TODO: dodelat "dojizdeni" emitteru - cas po ktery uz nebudou vydavany castice, ale
+    //stare castice se budou stale pohybovat, az zmizi. Po tu dobu vracet stale true!
+
+    //Zde update vsech castic a emit novych
+    //...
+
     return true; //false pokud emitter dosahne maximalniho casu pusobeni nebo je jinak odstranen
 }
 
@@ -15,24 +38,57 @@ EmitterMgr::~EmitterMgr()
     //TODO: uvolneni pameti (vector emitteru)
 }
 
-void EmitterMgr::AddEmitter(float x, float y, float z, float dirangleh, float diranglev, float angleh, float anglev, unsigned int textureId, float minrange, float maxrange, unsigned int time, unsigned int speed, unsigned int velocity, bool gravity)
+void EmitterMgr::AddEmitter(float x, float y, float z,
+                    float dirangleh, float diranglev, float angleh, float anglev,
+                    unsigned int modelId,
+                    float minrange, float maxrange,
+                    unsigned int time, unsigned int speed, unsigned int velocity,
+                    bool gravity)
 {
-    //TODO: DO
+    ParticleEmitter* pTemp = new ParticleEmitter();
+    pTemp->angleh = angleh;
+    pTemp->anglev = anglev;
+    pTemp->dirangleh = dirangleh;
+    pTemp->diranglev = diranglev;
+    pTemp->gravity = gravity;
+    pTemp->maxrange = maxrange;
+    pTemp->minrange = minrange;
+    pTemp->modelId = modelId;
+    pTemp->nextParticleCountdown = 0;
+    pTemp->speed = speed;
+    pTemp->time = time;
+    pTemp->velocity = velocity;
+    pTemp->x = x;
+    pTemp->y = y;
+    pTemp->z = z;
+
+    Emitters.push_back(pTemp);
 }
 
-void EmitterMgr::Update()
+void EmitterMgr::AddEmitter(ParticleEmitter* pEmitter)
+{
+    Emitters.push_back(pEmitter);
+}
+
+void EmitterMgr::Update(const clock_t diff)
 {
     if(Emitters.empty())
         return;
 
-    for(vector<ParticleEmitter*>::const_iterator itr = Emitters.begin(); itr != Emitters.end(); ++itr)
+    static list<ParticleEmitter*> RemoveList;
+    RemoveList.clear();
+
+    for(list<ParticleEmitter*>::const_iterator itr = Emitters.begin(); itr != Emitters.end(); ++itr)
     {
-        if(!(*itr)->Update())
+        if(!(*itr)->Update(diff))
         {
             //Pridat emitter do "remove listu", aby se nenarusila iterace jeho mazanim
+            RemoveList.push_back(*itr);
         }
     }
 
-    //projit remove list a vymazat ukoncene emittery
+    //Projit remove list a vymazat zaznamy
+    for(list<ParticleEmitter*>::const_iterator itr = RemoveList.begin(); itr != RemoveList.end(); ++itr)
+        Emitters.remove(*itr);
 }
 
